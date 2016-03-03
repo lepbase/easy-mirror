3572e86275254adea08df5b8cc87d6db47bae97a
Richard Challis <rjchallis@gmail.com>
2016-03-03 09:46:16 +0000

    no need for --sudo flag in cpanm and only create WEB_USER_NAME if set

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -35,58 +35,58 @@ apt-get -y install git \
            cpanminus
 
 # install most required perl modules using cpanminus
-cpanm --sudo Archive::Zip \
-             CGI::Session \
-             Class::Accessor \
-             CSS::Minifier \
-             DBI \
-             HTTP::Date \
-             Image::Size \
-             Inline IO::Scalar \
-             IO::Socket \
-             IO::Socket::INET \
-             IO::Socket::UNIX \
-             IO::String \
-             List::MoreUtils \
-             Mail::Mailer \
-             Math::Bezier \
-             MIME::Types \
-             PDF::API2 \
-             RTF::Writer \
-             Spreadsheet::WriteExcel \
-             Sys::Hostname::Long \
-             Text::ParseWords \
-             URI \
-             URI::Escape \
-             HTML::Template \
-             Clone \
-             Hash::Merge \
-             Class::DBI::Sweet \
-             Compress::Bzip2 \
-             Digest::MD5 \
-             File::Spec::Functions \
-             HTML::Entities \
-             IO::Uncompress::Bunzip2 \
-             XML::Parser \
-             XML::Writer \
-             SOAP::Lite \
-             GD \
-             GraphViz \
-             String::CRC32 \
-             Cache::Memcached::GetParser \
-             Inline::C \
-             XML::Atom \
-             LWP \
-             BSD::Resource \
-             WWW::Curl::Multi \
-             JSON \
-             Linux::Pid \
-             Readonly \
-             Module::Build \
-             Bio::Root::Build \
-             Lingua::EN::Inflect \
-             YAML \
-             Math::Round
+cpanm  Archive::Zip \
+       CGI::Session \
+       Class::Accessor \
+       CSS::Minifier \
+       DBI \
+       HTTP::Date \
+       Image::Size \
+       Inline IO::Scalar \
+       IO::Socket \
+       IO::Socket::INET \
+       IO::Socket::UNIX \
+       IO::String \
+       List::MoreUtils \
+       Mail::Mailer \
+       Math::Bezier \
+       MIME::Types \
+       PDF::API2 \
+       RTF::Writer \
+       Spreadsheet::WriteExcel \
+       Sys::Hostname::Long \
+       Text::ParseWords \
+       URI \
+       URI::Escape \
+       HTML::Template \
+       Clone \
+       Hash::Merge \
+       Class::DBI::Sweet \
+       Compress::Bzip2 \
+       Digest::MD5 \
+       File::Spec::Functions \
+       HTML::Entities \
+       IO::Uncompress::Bunzip2 \
+       XML::Parser \
+       XML::Writer \
+       SOAP::Lite \
+       GD \
+       GraphViz \
+       String::CRC32 \
+       Cache::Memcached::GetParser \
+       Inline::C \
+       XML::Atom \
+       LWP \
+       BSD::Resource \
+       WWW::Curl::Multi \
+       JSON \
+       Linux::Pid \
+       Readonly \
+       Module::Build \
+       Bio::Root::Build \
+       Lingua::EN::Inflect \
+       YAML \
+       Math::Round
 
 # force install DBD::mysql as it will fail tests with default dbuser and password
 cpanm --force DBD::mysql
@@ -120,17 +120,21 @@ cd $CURRENTDIR
 # create symbolic link to perl binary in location referenced by ensembl scripts
 ln -s /usr/bin/perl /usr/local/bin/perl
 
-# create user $WEB_USER_NAME
-WEB_USER_NAME=$(awk -F "=" '/WEB_USER_NAME/ {print $2}' $INI | tr -d ' ')
-WEB_USER_PASS=$(awk -F "=" '/WEB_USER_PASS/ {print $2}' $INI | tr -d ' ')
-addgroup $WEB_USER_NAME
-adduser $WEB_USER_NAME \
-        --ingroup $WEB_USER_NAME \
-        --gecos "First Last,RoomNumber,WorkPhone,HomePhone" \
-        --disabled-password
-echo "$WEB_USER_NAME:$WEB_USER_PASS" | chpasswd
-
-# create a directory for the ensembl code and change ownership to eguser
+# create a directory for the ensembl code
 SERVER_ROOT=$(awk -F "=" '/SERVER_ROOT/ {print $2}' $INI | tr -d ' ')
 mkdir $SERVER_ROOT
-chown $WEB_USER_NAME:$WEB_USER_NAME $SERVER_ROOT
+
+# create user $WEB_USER_NAME if set
+WEB_USER_NAME=$(awk -F "=" '/WEB_USER_NAME/ {print $2}' $INI | tr -d ' ')
+WEB_USER_PASS=$(awk -F "=" '/WEB_USER_PASS/ {print $2}' $INI | tr -d ' ')
+if ! [ -z $WEB_USER_NAME ]; then
+  addgroup $WEB_USER_NAME
+  adduser $WEB_USER_NAME \
+          --ingroup $WEB_USER_NAME \
+          --gecos "First Last,RoomNumber,WorkPhone,HomePhone" \
+          --disabled-password
+  echo "$WEB_USER_NAME:$WEB_USER_PASS" | chpasswd
+
+  # change ownership of $SERVER_ROOT to eguser
+  chown $WEB_USER_NAME:$WEB_USER_NAME $SERVER_ROOT
+fi
