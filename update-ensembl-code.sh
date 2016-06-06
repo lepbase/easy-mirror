44a20437422a528b1b56e96e1d6077f087774524
Richard Challis <rjchallis@gmail.com>
2016-06-06 16:21:29 +0100

    update conditionals

--- update-ensembl-code.sh
+++ update-ensembl-code.sh
@@ -364,18 +364,16 @@ do
   else
     echo "Connection to $DB on $TEST_HOST successful"
     LC_COLLATE=C
-    if [ $INDEX -eq 1 ]; then
+    if [[ $INDEX -eq 1 ]]; then
       UC_TYPE="COMPARA"
+    elif [[ $INDEX -eq 2 ]]; then
+      UC_TYPE="COMPARA_PAN_ENSEMBL"
     else
-      if [ $INDEX -eq 2 ]; then
-        UC_TYPE="COMPARA_PAN_ENSEMBL"
-      else
-        echo "WARNING: nothing to be done with database $DB"
-      fi
+      echo "WARNING: nothing to be done with database $DB"
     fi
     # add database connection parameters to Genus_species.ini
     printf "\n[DATABASE_$UC_TYPE]\nHOST = $TEST_HOST\nPORT = $TEST_PORT\nUSER = $TEST_USER\nPASS = $TEST_PASS\n" >> $SERVER_ROOT/public-plugins/mirror/conf/ini-files/MULTI.ini
     perl -p -i -e "s/(.OTHER_DATABASES)/DATABASE_$UC_TYPE = $DB\n\$1/" $SERVER_ROOT/public-plugins/mirror/conf/ini-files/MULTI.ini
   fi
-  INDEX=$INDEX+1
+  ((INDEX++))
 done
